0ad9fc8a79d1c5ba53d89c793c823789be3f12ba
Esha522 <eshaaslam659@gmail.com>
2025-08-08 17:00:50 +0500

    Update for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const serverless = require('serverless-http');
 
 dotenv.config();
 connectDB();
