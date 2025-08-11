afb90dee599265e53fca47444aa910a568b2d1e6
Esha522 <eshaaslam659@gmail.com>
2025-08-11 11:31:28 +0500

    Update customerroutes.js

--- Routes/customerroutes.js
+++ Routes/customerroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../../../middleware/auth');
+const { protect, admin } = require('../middleware/auth');
 const {getAllCustomers, createCustomer, updateCustomer, deleteCustomer} = require ('../Controllers/customer');
 
 router.get('/', protect, admin, getAllCustomers )
