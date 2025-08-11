7a63cd04ed57bd7b41bb5a5795069721720fd224
Esha522 <eshaaslam659@gmail.com>
2025-08-11 11:33:03 +0500

    Update servicemanagementroutes.js

--- Routes/servicemanagementroutes.js
+++ Routes/servicemanagementroutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createService, getAllServices, deleteService} = require('../Controllers/servicemanagement');
-const { protect, admin } = require('../../../middleware/auth');
+const { protect, admin } = require('../middleware/auth');
 
 router.get('/', protect, getAllServices);
 router.post('/create', protect, admin, createService);
