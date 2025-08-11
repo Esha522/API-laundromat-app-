0384953e202b56d85a3afd5935560e46e4b09c33
Esha522 <eshaaslam659@gmail.com>
2025-08-11 11:33:19 +0500

    Update transactionroutes.js

--- Routes/transactionroutes.js
+++ Routes/transactionroutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {createTransaction, getAllTransactions, getTransactionById } = require('../Controllers/transaction');
-const { protect, admin } = require('../../../middleware/auth');
+const { protect, admin } = require('../middleware/auth');
 
 
 router.post('/create', protect , admin, createTransaction);
